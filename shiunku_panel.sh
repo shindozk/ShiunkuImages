9ef30eb641b92f0155846ac451e0cfebf04c373b
ShindoZk <77684277+shindozk@users.noreply.github.com>
2025-03-13 22:22:55 -0300

    Create shiunku_panel.sh

---
+++ shiunku_panel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+Shiunku Panel Installer v0.0.5
+
+# Clone the repository and navigate into it
+echo "🚀 Cloning the Shiunku Panel repository..."
+git clone https://github.com/shindozk/Shiunku.git && cd Shiunku
+
+if [ $? -ne 0 ]; then
+  echo "❌ Failed to clone or change directory to Shiunku."
+  exit 1
+fi
+
+# Install Node.js dependencies
+echo "📦 Installing dependencies..."
+npm install
+
+# Seed the database
+echo "🌱 Seeding the database..."
+npm run seed
+
+# Create an administrator user
+echo "👤 Creating an administrator user..."
+npm run createUser
+
+# Start the Shiunku Panel
+echo "🚀 Starting Shiunku Panel..."
+node .
